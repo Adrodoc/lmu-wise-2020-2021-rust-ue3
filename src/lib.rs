f3995f4dbb0e746c455adf09b7bb7c5c0d9d0264
Adrodoc <adrodoc55@googlemail.com>
2020-11-25 10:56:45 +0100

    Add Polynom.add_term

--- src/lib.rs
+++ src/lib.rs
@@ -36,6 +36,29 @@ impl std::fmt::Display for Polynom {
 }
 
 impl Polynom {
+    fn new() -> Polynom {
+        Polynom::Empty
+    }
+
+    fn add_term(self, coefficient: f64, exponent: i32) -> Polynom {
+        match self {
+            Polynom::Empty => Polynom::Full {
+                coefficient,
+                exponent,
+                next: Box::new(Polynom::Empty),
+            },
+            Polynom::Full {
+                coefficient: c,
+                exponent: e,
+                next,
+            } => Polynom::Full {
+                coefficient: c,
+                exponent: e,
+                next: Box::new(next.add_term(coefficient, exponent)),
+            },
+        }
+    }
+
     fn eval(&self, x: f64) -> f64 {
         match self {
             Polynom::Empty => 0.,
@@ -92,25 +115,95 @@ mod tests {
     const PRECISION: f64 = 0.000001;
 
     #[test]
+    fn print_empty_polynom() {
+        // given:
+        let under_test = Polynom::new();
+
+        // when:
+        let actual = under_test.to_string();
+
+        // then:
+        assert_eq!(actual, "");
+    }
+
+    // let under_test = Polynom::Full {
+    //     coefficient: 1.,
+    //     exponent: 3,
+    //     next: Box::new(Polynom::Full {
+    //         coefficient: -2.,
+    //         exponent: 2,
+    //         next: Box::new(Polynom::Full {
+    //             coefficient: -11.,
+    //             exponent: 1,
+    //             next: Box::new(Polynom::Full {
+    //                 coefficient: 12.,
+    //                 exponent: 0,
+    //                 next: Box::new(Polynom::Empty),
+    //             }),
+    //         }),
+    //     }),
+    // };
+
+    #[test]
+    fn print_polynoms() {
+        // given:
+        let under_test = Polynom::new()
+            .add_term(1., 3)
+            .add_term(2., 2)
+            .add_term(-11., 1)
+            .add_term(12., 0);
+
+        // when:
+        let actual = under_test.to_string();
+
+        // then:
+        assert_eq!(actual, "1x^3 + 2x^2 -11x + 12");
+    }
+
+    #[test]
+    fn eval_polynoms() {
+        // given:
+        let under_test = Polynom::new()
+            .add_term(-5., 3)
+            .add_term(2., 2)
+            .add_term(-4., 1)
+            .add_term(12., 0);
+
+        // when:
+        let actual = under_test.eval(2.);
+        // then:
+        assert_eq!(actual, -28.);
+
+        // when:
+        let actual = under_test.eval(0.);
+        // then:
+        assert_eq!(actual, 12.);
+    }
+
+    #[test]
+    fn differentiate_polynoms() {
+        // given:
+        let under_test = Polynom::new()
+            .add_term(-1., 3)
+            .add_term(2., 2)
+            .add_term(-11., 1)
+            .add_term(12., 0);
+
+        // when:
+        let actual = under_test.differentiate();
+
+        // then:
+        assert_eq!(actual.to_string(), "-3x^2 + 4x -11");
+    }
+    #[test]
     fn find_root_exercise_sheet_first_test() {
         // given
-        let under_test = Polynom::Full {
-            coefficient: 1.,
-            exponent: 3,
-            next: Box::new(Polynom::Full {
-                coefficient: -2.,
-                exponent: 2,
-                next: Box::new(Polynom::Full {
-                    coefficient: -11.,
-                    exponent: 1,
-                    next: Box::new(Polynom::Full {
-                        coefficient: 12.,
-                        exponent: 0,
-                        next: Box::new(Polynom::Empty),
-                    }),
-                }),
-            }),
-        };
+        let under_test = Polynom::new()
+            .add_term(1., 3)
+            .add_term(-2., 2)
+            .add_term(-11., 1)
+            .add_term(12., 0);
+
         // when:
         let actual = under_test.find_root(-4.);
         // then:
@@ -130,23 +223,12 @@ mod tests {
     #[test]
     fn find_root_exercise_sheet_second_test() {
         // given
-        let under_test = Polynom::Full {
-            coefficient: 1.,
-            exponent: 3,
-            next: Box::new(Polynom::Full {
-                coefficient: -2.,
-                exponent: 2,
-                next: Box::new(Polynom::Full {
-                    coefficient: -5.,
-                    exponent: 1,
-                    next: Box::new(Polynom::Full {
-                        coefficient: 6.,
-                        exponent: 0,
-                        next: Box::new(Polynom::Empty),
-                    }),
-                }),
-            }),
-        };
+        let under_test = Polynom::new()
+            .add_term(1., 3)
+            .add_term(-2., 2)
+            .add_term(-5., 1)
+            .add_term(6., 0);
+
         // when:
         let actual = under_test.find_root(-3.);
         // then:
@@ -166,27 +248,13 @@ mod tests {
     #[test]
     fn find_root_exercise_sheet_third_test() {
         // given
-        let under_test = Polynom::Full {
-            coefficient: 2.,
-            exponent: 4,
-            next: Box::new(Polynom::Full {
-                coefficient: 7.,
-                exponent: 3,
-                next: Box::new(Polynom::Full {
-                    coefficient: 6.,
-                    exponent: 2,
-                    next: Box::new(Polynom::Full {
-                        coefficient: 8.,
-                        exponent: 1,
-                        next: Box::new(Polynom::Full {
-                            coefficient: 12.,
-                            exponent: 0,
-                            next: Box::new(Polynom::Empty),
-                        }),
-                    }),
-                }),
-            }),
-        };
+        let under_test = Polynom::new()
+            .add_term(2., 4)
+            .add_term(7., 3)
+            .add_term(6., 2)
+            .add_term(8., 1)
+            .add_term(12., 0);
+
         // when:
         let actual = under_test.find_root(0.);
         // then:
@@ -197,105 +265,6 @@ mod tests {
         // then:
         assert_approx_eq!(actual, -2.5943, PRECISION);
         // TEST IS WRONG!!!
-    }
-
-    #[test]
-    fn print_empty_polynom() {
-        // given:
-        let empty_poly = Polynom::Empty;
-
-        // when:
-        let actual = empty_poly.to_string();
-
-        // then:
-        assert_eq!(actual, "");
-    }
-
-    #[test]
-    fn print_polynoms() {
-        // given:
-        let under_test = Polynom::Full {
-            coefficient: 1.,
-            exponent: 3,
-            next: Box::new(Polynom::Full {
-                coefficient: 2.,
-                exponent: 2,
-                next: Box::new(Polynom::Full {
-                    coefficient: -11.,
-                    exponent: 1,
-                    next: Box::new(Polynom::Full {
-                        coefficient: 12.,
-                        exponent: 0,
-                        next: Box::new(Polynom::Empty),
-                    }),
-                }),
-            }),
-        };
-
-        // when:
-        let actual = under_test.to_string();
-
-        // then:
-        assert_eq!(actual, "1x^3 + 2x^2 -11x + 12");
-    }
-
-    #[test]
-    fn eval_polynoms() {
-        // given:
-        let under_test = Polynom::Full {
-            coefficient: -5.,
-            exponent: 3,
-            next: Box::new(Polynom::Full {
-                coefficient: 2.,
-                exponent: 2,
-                next: Box::new(Polynom::Full {
-                    coefficient: -4.,
-                    exponent: 1,
-                    next: Box::new(Polynom::Full {
-                        coefficient: 12.,
-                        exponent: 0,
-                        next: Box::new(Polynom::Empty),
-                    }),
-                }),
-            }),
-        };
-
-        // when:
-        let actual = under_test.eval(2.);
-        // then:
-        assert_eq!(actual, -28.);
-
-        // when:
-        let actual = under_test.eval(0.);
-        // then:
-        assert_eq!(actual, 12.);
-    }
-
-    #[test]
-    fn differentiate_polynoms() {
-        // given:
-        let under_test = Polynom::Full {
-            coefficient: -1.,
-            exponent: 3,
-            next: Box::new(Polynom::Full {
-                coefficient: 2.,
-                exponent: 2,
-                next: Box::new(Polynom::Full {
-                    coefficient: -11.,
-                    exponent: 1,
-                    next: Box::new(Polynom::Full {
-                        coefficient: 12.,
-                        exponent: 0,
-                        next: Box::new(Polynom::Empty),
-                    }),
-                }),
-            }),
-        };
-
-        // when:
-        let actual = under_test.differentiate();
-
-        // then:
-        assert_eq!(actual.to_string(), "-3x^2 + 4x -11");
+        // assert_approx_eq!(actual, -1.5, PRECISION);
     }
 }
